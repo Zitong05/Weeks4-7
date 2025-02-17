8c29b232d18e3283588286dbe5de2bf169e6ab4c
Zitong05 <wangzito@sheridancollege.ca>
2025-02-17 01:52:18 -0500

    Health bar (No if statement for take damage)

---
+++ Assets/Exploreable Toy Scripts/Health Bar.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public float health = 5;
+    public Slider enemyhealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float clampedValue = Mathf.Clamp(health, 0f, 5f);
+        enemyhealth.minValue = 0;
+        enemyhealth.maxValue = health;
+        enemyhealth.value = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ()
+        {
+            TakeDamage(1)
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        enemyhealth.value = health;
+    }
+}
