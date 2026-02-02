95eb7cbc9a39891f7012f20d5b736a9e3bec791b
Zitong Wang <wangzito@shernet.sheridancollege.ca>
2026-02-02 09:54:17 -0500

    Checked if mouse is over UI

---
+++ Assets/Scripts/Duckie.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class Duckie : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //was the mouse pressed this frame? and not over the UI
+        if(Mouse.current.leftButton.wasPressedThisFrame == true && !EventSystem.current.IsPointerOverGameObject())
+        {
+            //Y: mouse our tranform position to the mouse position
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint( Mouse.current.position.ReadValue());
+            transform.position = mousePos;
+        }
+        
+    }
+}
