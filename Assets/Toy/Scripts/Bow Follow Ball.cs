01df5b06aa965341f55ccc4753f2bd1736666e8c
Zitong05 <wangzito@sheridancollege.ca>
2026-02-14 01:23:46 -0500

    UI

--- Assets/Toy/Scripts/Bow Follow Ball.cs
+++ Assets/Toy/Scripts/Bow Follow Ball.cs
@@ -5,7 +5,7 @@ public class BowFollowBall : MonoBehaviour
 {
     public Transform ball;
     Vector3 direction;
-    Vector3 bowPosition;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
